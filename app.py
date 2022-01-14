6367ba462f212213e324fad71cf0b8f2e9dd0c06
sy <phzzz@naver.com>
2022-01-14 14:16:20 +0900

    temp_app.py_result_api

--- app.py
+++ app.py
@@ -7,51 +7,35 @@ import numpy as np
 from PIL import Image
 import os
 
-
-
-
-#model = tf.keras.models.load_model('static/model/first_training_data.h5')
-#print("model roaded")
-
-
-
-
-app = Flask(__name__)
-
-from pymongo import MongoClient
-
-
-
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
-app = Flask(__name__)
-
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.i0lgb.mongodb.net/test')
-db = client.dbprojects
-
-
-#SY db 테스트용
-client = MongoClient('mongodb+srv://test:sparta@cluster0.g1cco.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.dbsidepj
-
-
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://music.bugs.co.kr/musicpd/albumview/39691',headers=headers) # 기쁠 때
-data2 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50746',headers=headers) # 화날 때
-data3 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50519',headers=headers) # 짜증날 때
-data4 = requests.get('https://music.bugs.co.kr/musicpd/albumview/41409',headers=headers) # 무서울 때
-data5 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50793',headers=headers) # 무표정일 때
-data6 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50839',headers=headers) # 슬플 때
-data7 = requests.get('https://music.bugs.co.kr/musicpd/albumview/44402?wl_ref=list_mab_01',headers=headers) # 놀랄 때
+from pymongo import MongoClient
+
+# model = tf.keras.models.load_model('static/model/first_training_data.h5')
+# print("model roaded")
 
 
-check = list(db.userinfo.find({'feeling_num':0},{'_id':False}))
-if len(check) == 0:
+# client = MongoClient('mongodb+srv://test:sparta@cluster0.i0lgb.mongodb.net/test')
+# db = client.dbprojects
+
+
+# SY db 테스트용
+client = MongoClient('mongodb+srv://test:sparta@cluster0.g1cco.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+db = client.dbsidepj
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://music.bugs.co.kr/musicpd/albumview/39691', headers=headers)  # 기쁠 때
+data2 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50746', headers=headers)  # 화날 때
+data3 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50519', headers=headers)  # 짜증날 때
+data4 = requests.get('https://music.bugs.co.kr/musicpd/albumview/41409', headers=headers)  # 무서울 때
+data5 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50793', headers=headers)  # 무표정일 때
+data6 = requests.get('https://music.bugs.co.kr/musicpd/albumview/50839', headers=headers)  # 슬플 때
+data7 = requests.get('https://music.bugs.co.kr/musicpd/albumview/44402?wl_ref=list_mab_01', headers=headers)  # 놀랄 때
+
+check = db.feeling_data.find_one({'feeling_num': 0})
+if check is None:
     soup_happiness = BeautifulSoup(data.text, 'html.parser')
     soup_angry = BeautifulSoup(data2.text, 'html.parser')
     song_disgust = BeautifulSoup(data3.text, 'html.parser')
@@ -59,7 +43,6 @@ if len(check) == 0:
     song_neutral = BeautifulSoup(data5.text, 'html.parser')
     song_sad = BeautifulSoup(data6.text, 'html.parser')
     song_surprise = BeautifulSoup(data7.text, 'html.parser')
-
 
     # 데이터
     emotion_musics = [soup_happiness.select('#ESALBUM39691 > table > tbody > tr'),
@@ -87,15 +70,18 @@ if len(check) == 0:
             singer_list.append(singer)
 
         db.feeling_data.insert_one({
-            'feeing_num': idx,
+            'feeling_num': idx,
             'color': color[idx],
             'img': img_list,
             'title': title_list,
             'singer': singer_list,
             # 'chicken': chicken
-            #'movie' : movie
+            # 'movie' : movie
         })
-        #print("db넣었다!")
+
+
+
+app = Flask(__name__)
 
 
 @app.route('/first')
@@ -107,44 +93,44 @@ def home():
 def result():
     return render_template('result.html')
 
-@app.route('/result/api', methods=['GET'])
+
+@app.route('/result/api', methods=['POST'])
 def result_api():
     # 이미지이기에, rescale 및 size 조정을 위해 ImageDataGenerator 활용
     test_datagen = ImageDataGenerator(rescale=1. / 255)
-    test_dir = 'static/img/'
+    test_dir = 'static/img/'     # test_dir에 폴더별로 사진을 저장 해야함
 
-    test_generator = test_datagen.flow_from_directory( # test_dir에 폴더별로 저장을 해야함
+    test_generator = test_datagen.flow_from_directory(
         test_dir,
         target_size=(48, 48),
         shuffle=False,
         class_mode='categorical'
     )
-    pred = model.predict(test_generator)[0].tolist()  # numpyarray를 list로 변환
-    # 마지막으로 업로드한 사진에 대한 판별결과를 보여줌
-    pred_sorted = []
-    for idx, val in enumerate(pred):
-        pred_sorted.append((idx, val))  # 인덱스번호 부여
-    pred_sorted.sort(reverse=True, key=lambda x: x[1])  # 원래의 값을 기준으로 내림차순 정렬
+    # pred = model.predict(test_generator)[0].tolist()  # numpyarray를 list로 변환
+    # # 마지막으로 업로드한 사진에 대한 판별결과를 보여줌
+    # pred_sorted = []
+    # for idx, val in enumerate(pred):
+    #     pred_sorted.append((idx, val))  # 인덱스번호 부여
+    # pred_sorted.sort(reverse=True, key=lambda x: x[1])  # 원래의 값을 기준으로 내림차순 정렬
 
-    result = []
-    fir_data = db.feeling_data.find_one({'feeling_num':pred_sorted[0][0]})  # 1번째로 높은 값 가져오기
-    result.append(fir_data)
+    result = list(db.feeling_data.find({}, {'_id': False}))  # 임시로 모든 데이터를 넣었습니다!
 
-    sec_data = db.feeling_data.find_one({'feeling_num':pred_sorted[1][0]})  # 2번째로 높은 감정의 값들 가져오기
-    result.append(sec_data)
+    # fir_data = db.feeling_data.find_one({'feeling_num':pred_sorted[0][0]})  # 1번째로 높은 값 가져오기
+    # result.append(fir_data)
+    #
+    # sec_data = db.feeling_data.find_one({'feeling_num':pred_sorted[1][0]})  # 2번째로 높은 감정의 값들 가져오기
+    # result.append(sec_data)
+    #
+    # thd_data = db.feeling_data.find_one({'feeling_num':pred_sorted[2][0]})  # 3번째로 높은 감정의 값들 가져오기
+    # result.append(thd_data)
 
-    thd_data = db.feeling_data.find_one({'feeling_num':pred_sorted[2][0]})  # 3번째로 높은 감정의 값들 가져오기
-    result.append(thd_data)
-
-    print(result)
     return result
 
 
-@app.route('result/second')
+@app.route('/result/second')
 def result_second():
     return render_template('second.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
-
+    app.run('0.0.0.0', port=5001, debug=False)
