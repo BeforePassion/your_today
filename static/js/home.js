7ed08550c5a856c44bebb40a9fed7d779d56c5c3
csd <Godchoi96>
2022-01-14 20:16:44 +0900

    Finished SN Branch Front-end

--- static/js/home.js
+++ static/js/home.js
@@ -36,7 +36,7 @@ function posting() {
             alert(response["result"])
             // 아래처럼 하지 않아도, 백엔드(app.py)에서 바로 판별 함수를 실행한 뒤에
             // render_template 을 해서 바로 결과 페이지로 넘어가도 됨
-            window.location.href='/result'
+
         }
     });
   }
