943c92dbcbe3fbb5e3c2d58b51fe21a7a416dcff
sy <phzzz@naver.com>
2022-01-14 11:45:45 +0900

    app.py db_Load update

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ data7 = requests.get('https://music.bugs.co.kr/musicpd/albumview/44402?wl_ref=li
 
 
 check = list(db.userinfo.find({'feeling_num':0},{'_id':False}))
-if len(check) != 1:
+if len(check) == 0:
     soup_happiness = BeautifulSoup(data.text, 'html.parser')
     soup_angry = BeautifulSoup(data2.text, 'html.parser')
     song_disgust = BeautifulSoup(data3.text, 'html.parser')
@@ -62,123 +62,53 @@ if len(check) != 1:
 
 
     # 데이터
-    happiness = soup_happiness.select('#ESALBUM39691 > table > tbody > tr')
-    for song_happiness in happiness:
-        happiness_img = song_happiness.select_one('td > a > img')['src']
-        happiness_title = song_happiness.select_one('th > p > a').text
-        happiness_singer = song_happiness.select_one('td > p > a').text
-        db.feeling_data.insert_one =({
-            'feeing_num': 0,
-            'color':'red',
-            'img' :happiness_img,
-            'title': happiness_title,
-            'singer': happiness_singer,
-            #'chicken': chicken
-        })
+    emotion_musics = [soup_happiness.select('#ESALBUM39691 > table > tbody > tr'),
+                      soup_angry.select('#ESALBUM50746 > table > tbody > tr'),
+                      song_fear.select('#ESALBUM50519 > table > tbody > tr'),
+                      song_fear.select('#ESALBUM41409 > table > tbody > tr'),
+                      song_neutral.select('#ESALBUM50793 > table > tbody > tr'),
+                      song_sad.select('#ESALBUM50839 > table > tbody > tr'),
+                      song_surprise.select('#ESALBUM44402 > table > tbody > tr'),
+                      ]
 
-        # print(happiness_img, happiness_title, happiness_singer)
+    color = ['yellow', 'red', 'pink', 'black', 'gray', 'purple', 'blue']
+    for idx, emotion in enumerate(emotion_musics):  # emotion == happiness // detail_song == song_happiness
+        img_list = []
+        title_list = []
+        singer_list = []
 
-    angry = soup_angry.select('#ESALBUM50746 > table > tbody > tr')
-    for song_angry in angry:
-        angry_img = song_angry.select_one('td > a > img')['src']
-        angry_title = song_angry.select_one('th > p > a').text
-        angry_singer = song_angry.select_one('td > p > a').text
-        # print(angry_img, angry_title, angry_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 1,
-            'color': 'red',
-            'img': angry_img,
-            'title': angry_title,
-            'singer': angry_singer,
+        for detail_song in emotion:
+            img = detail_song.select_one('td > a > img')['src']
+            title = detail_song.select_one('th > p > a').text
+            singer = detail_song.select_one('td > p > a').text
+
+            img_list.append(img)
+            title_list.append(title)
+            singer_list.append(singer)
+
+        db.feeling_data.insert_one({
+            'feeing_num': idx,
+            'color': color[idx],
+            'img': img_list,
+            'title': title_list,
+            'singer': singer_list,
             # 'chicken': chicken
+            #'movie' : movie
         })
+        #print("db넣었다!")
 
-    disgust = song_fear.select('#ESALBUM50519 > table > tbody > tr')
-    for song_disgust in disgust:
-        disgust_img = song_disgust.select_one('td > a > img')['src']
-        disgust_title = song_disgust.select_one('th > p > a').text
-        disgust_singer = song_disgust.select_one('td > p > a').text
-        # print(disgust_img, disgust_title, disgust_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 2,
-            'color': 'pink',
-            'img': disgust_img,
-            'title': disgust_title,
-            'singer': disgust_singer,
-            # 'chicken': chicken
-        })
 
-    fear = song_fear.select('#ESALBUM41409 > table > tbody > tr')
-    for song_fear in fear:
-        fear_img = song_fear.select_one('td > a > img')['src']
-        fear_title = song_fear.select_one('th > p > a').text
-        fear_singer = song_fear.select_one('td > p > a').text
-        # print(fear_img, fear_title, fear_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 3,
-            'color': 'black',
-            'img': fear_img,
-            'singer': fear_singer,
-            'title': fear_title,
-            # 'chicken': chicken
-        })
-
-    neutral = song_neutral.select('#ESALBUM50793 > table > tbody > tr')
-    for song_neutral in neutral:
-        neutral_img = song_neutral.select_one('td > a > img')['src']
-        neutral_title = song_neutral.select_one('th > p > a').text
-        neutral_singer = song_neutral.select_one('td > p > a').text
-        # print(neutral_img, neutral_title, neutral_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 4,
-            'color': 'gray',
-            'img': neutral_img,
-            'title': neutral_singer,
-            'singer': neutral_singer,
-            # 'chicken': chicken
-        })
-
-    sad = song_sad.select('#ESALBUM50839 > table > tbody > tr')
-    for song_sad in sad:
-        sad_img = song_sad.select_one('td > a > img')['src']
-        sad_title = song_sad.select_one('th > p > a').text
-        sad_singer = song_sad.select_one('td > p > a').text
-        # print(sad_img, sad_title, sad_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 5,
-            'color': 'purple',
-            'img': sad_img,
-            'title':sad_title,
-            'singer':sad_singer,
-            # 'chicken': chicken
-        })
-
-    surprise = song_surprise.select('#ESALBUM44402 > table > tbody > tr')
-    for song_surprise in surprise:
-        surprise_img = song_surprise.select_one('td > a > img')['src']
-        surprise_title = song_surprise.select_one('th > p > a').text
-        surprise_singer = song_surprise.select_one('td > p > a').text
-        # print(surprise_img, surprise_title, surprise_singer)
-        db.feeling_data.insert_one = ({
-            'feeing_num': 6,
-            'color': 'blue',
-            'img': surprise_img,
-            'title': surprise_title,
-            'singer': surprise_singer
-            # 'chicken': chicken
-        })
-
-@app.route('/')
+@app.route('/first')
 def home():
-    return render_template('index.html')
+    return render_template('first.html')
 
 
 @app.route('/result')
 def result():
     return render_template('result.html')
 
-@app.route('/result/recommend', methods=['GET'])
-def result_recommend():
+@app.route('/result/api', methods=['GET'])
+def result_api():
     # 이미지이기에, rescale 및 size 조정을 위해 ImageDataGenerator 활용
     test_datagen = ImageDataGenerator(rescale=1. / 255)
     test_dir = 'static/img/'
